4509c6f8d7c450db8a838871bc8cb0520c558d66
rudolfvegh <tankyboy@gmail.com>
2025-05-23 08:31:11 +0200

    report printing and import sys added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from stats import word_count, character_count
+import sys
+
+from stats import (
+    word_count, 
+    character_count, 
+    sorted_list
+)
 
 def get_book_text(filepath):
     file_contents = ""
@@ -7,11 +13,22 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_path="books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path=sys.argv[1]
     book=get_book_text(book_path)
     word_number=word_count(book)
     character_dictionary=character_count(book)
-    print(f"{word_number} words found in the document")
-    print(character_dictionary)
+    final_list = sorted_list(character_dictionary)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_number} total words")
+    print("--------- Character Count -------")
+    for a in final_list:
+        if a["char"].isalpha() == True:
+            print(f"{a["char"]}: {a["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,20 @@ def character_count(text):
             char_dict[s] = 1
     return char_dict
 
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_list(char_dict):
+    sorted_list=[]
+    for s in char_dict:
+        helper_dict = {}
+        helper_dict["char"] = s
+        helper_dict["num"] = char_dict[s]
+        sorted_list.append(helper_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+    
+
+
+
